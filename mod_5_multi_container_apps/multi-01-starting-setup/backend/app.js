a2b57fbe7dcaa1e2c10a2eb4637de5534a1756a2
sfgarcia <sfgarcia@uc.cl>
2024-02-11 18:32:50 -0300

    Dockerize app

--- mod_5_multi_container_apps/multi-01-starting-setup/backend/app.js
+++ mod_5_multi_container_apps/multi-01-starting-setup/backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://localhost:27017/course-goals',
+  'mongodb://mongo-db/course-goals',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
